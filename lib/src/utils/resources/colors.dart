3f8afc5aa2e56ee7e65852e3bd62456007c67d30
Karen Araqelyan <karen.araqelyan@hylink.am>
2025-03-22 22:29:00 +0400

    add app colors file

---
+++ lib/src/utils/resources/colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const backgroundLightColor = Colors.white;
+  static const backgroundDarkColor = Colors.black;
+
+  static const waveLightColor = Color(0xFFBFDBFE);
+  static const waveDarkColor = Color(0xFF1E293B);
+
+  static const loginAndSignUpButtonActiveLightColor = Color(0xFF1A5CFF);
+  static const loginAndSignUpButtonActiveDarkColor = Color(0xFF1A5CFF);
+  static const loginAndSignUpButtonInactiveLightColor = Colors.lightBlue;
+  static const loginAndSignUpButtonInactiveDarkColor = Color(0xFF1E293B);
+  static const loginAndSignUpButtonActiveTextLightColor = Colors.white;
+  static const loginAndSignUpButtonActiveTextDarkColor = Colors.white;
+  static const loginAndSignUpButtonInactiveTextLightColor = Color(0xFF262931);
+  static const loginAndSignUpButtonInactiveTextDarkColor = Colors.white;
+
+  static const borderLightColor = Color(0xFF4A55A7);
+  static const borderDarkColor = Color(0xFF6371DE);
+  static const hintTextLightColor = Color(0xFF181818);
+  static const hintTextDarkColor = Colors.white;
+
+  static const forgetButtonLightColor = Color(0xFF1A5CFF);
+  static const forgetButtonDarkColor = Colors.white;
+}
