7caebaf0f35e418746f00b17454d1f2036d9650d
Karen Araqelyan <karen.araqelyan@hylink.am>
2025-03-22 22:35:05 +0400

    add app texts file

---
+++ lib/src/utils/resources/texts.dart
@@ -0,0 +1,10 @@
+abstract class AppTexts {
+  AppTexts._();
+
+  static const usernameHintText = 'Username';
+  static const passwordHintText = 'Password';
+  static const emailHintText = 'Email';
+  static const loginButtonText = 'Login';
+  static const signUpButtonText = 'Sign-up';
+  static const forgotPasswordButtonText = 'Forgot password?';
+}
