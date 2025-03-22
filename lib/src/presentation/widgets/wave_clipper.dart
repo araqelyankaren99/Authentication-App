90cae16b168c42e164a435aa107b1c2bb4475c48
Karen Araqelyan <karen.araqelyan@hylink.am>
2025-03-22 20:23:13 +0400

    add wave clipper

---
+++ lib/src/presentation/widgets/wave_clipper.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height);
+
+    path.cubicTo(
+      size.width * 2/5,
+      size.height * 0.95,
+      size.width * 4/5,
+      size.height * 0.25,
+      size.width,
+      size.height,
+    );
+
+    path.lineTo(size.width, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) {
+    return false;
+  }
+}
